fb8d696363d6da5ba108f64e526fc2d048d8eee2
Matthew Le Nepveu <matthew@lenepveu.com>
2019-05-28 16:56:42 +1000

    Started working on Assessment Task 1 for Software Development 3 by doing a login system based of Wamp databases. I managed to complete most of it but need to do a logout script and check that I obtained the correct database from this computer.

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$dbUsername = "root";
+$dBPassword = "";
+$dBName = "loginsystem";
+
+$conn = mysqli_connect($servername, $dbUsername, $dBPassword, $dBName);
+
+if (!$conn)
+{
+	die("Connection failed: ".mysqli_connect_error());
+}

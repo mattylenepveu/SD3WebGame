58df6faf6f7d4d1fb5189d1b50d1db2c1371d376
mattylenepveu <matthew@lenepveu.com>
2019-05-28 22:34:35 +1000

    Filled out the login.inc.php script again as it wasn't filled out at all on last commit for some reason, and also completed the tutorial I did on login system by adding the logout.inc.php file.

--- signup.php
+++ signup.php
@@ -4,12 +4,50 @@
 
 	<main>
 	  <h1>Signup</h1>
+	  <?php
+		  if (isset($_GET['error']))
+		  {
+			  if ($_GET['error'] == "emptyfields")
+			  {
+				  echo '<p>Fill in all fields!</p>';
+			  }
+			  
+			  else if ($_GET['error'] == "invaliduidmail")
+			  {
+				  echo '<p>Invalid username and e-mail!</p>';
+			  }
+			  
+			  else if ($_GET['error'] == "invaliduid")
+			  {
+				  echo '<p>Invalid username!</p>';
+			  }
+			  
+			  else if ($_GET['error'] == "invalidmail")
+			  {
+				  echo '<p>Invalid e-mail!</p>';
+			  }
+			  
+			  else if ($_GET['error'] == "passwordcheck")
+			  {
+				  echo '<p>Your passwords do not match!</p>';
+			  }
+			  
+			  else if ($_GET['error'] == "usertaken")
+			  {
+				  echo '<p>Signup successful!</p>';
+			  }
+		  }
+		  else if ($_GET['signup'] == success)
+		  {
+			  
+		  }
+	  ?>
 	  <form class="form-signup" action="includes/signup.inc.php" method="post">
-		<input type="text" name="uid" placeholder="Username">
-		<input type="text" name="mail" placeholder="E-mail">
-		<input type="password" name="pwd" placeholder="Password">
-		<input type="password" name="pwd-repeat" placeholder="Repeat password">
-		<button type="submit" name="signup-submit">Signup</button>
+		  <input type="text" name="uid" placeholder="Username">
+		  <input type="text" name="mail" placeholder="E-mail">
+		  <input type="password" name="pwd" placeholder="Password">
+		  <input type="password" name="pwd-repeat" placeholder="Repeat password">
+		  <button type="submit" name="signup-submit">Signup</button>
 	  </form>
 	</main>
 	
